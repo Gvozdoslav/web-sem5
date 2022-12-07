c10d679ce0910eef3a51e14d97747255d8576544
Gvozdoslav <remrostrus2123@yandex.ru>
2022-12-08 00:47:29 +0300

    feat: align spinner menu and create an animation for it

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -2,29 +2,33 @@ let currentIndex = 0
 const j = 3
 
 const dots = document.getElementsByClassName("shop-box-spinner__page")
-const images = document.getElementsByClassName('shop-box-spinner__img')
+const offers = document.getElementsByClassName("shop-box-spinner-items")[0].getElementsByTagName("li")
 
 window.addEventListener("load", init)
 
 function init() {
 
     for (let k = 0; k < j; k++) {
-        images[k].style['display'] = 'none'
+        offers[k].classList.replace("shop-box-spinner-item", "shop-box-spinner-item--inactive")
+        offers[k].style['display'] = 'none'
         dots[k].style['background-color'] = '#D5F8E6'
     }
 
-    images[0].style['display'] = 'flex'
+    offers[0].classList.replace("shop-box-spinner-item--inactive", "shop-box-spinner-item")
+    offers[0].style['display'] = 'flex'
     dots[0].style['background-color'] = '#a7ccba'
 }
 
 function next() {
 
     for (let k = 0; k < j; k++) {
-        images[k].style['display'] = 'none'
+        offers[k].classList.replace("shop-box-spinner-item", "shop-box-spinner-item--inactive")
+        offers[k].style['display'] = 'none'
         dots[k].style['background-color'] = '#D5F8E6'
     }
 
-    images[(currentIndex + 1) % j].style['display'] = 'flex'
+    offers[(currentIndex + 1) % j].classList.replace("shop-box-spinner-item--inactive", "shop-box-spinner-item")
+    offers[(currentIndex + 1) % j].style['display'] = 'flex'
     dots[(currentIndex + 1) % j].style['background-color'] = '#a7ccba'
 
     currentIndex = (currentIndex + 1) % j
@@ -33,11 +37,13 @@ function next() {
 function prev() {
 
     for (let k = 0; k < j; k++) {
-        images[k].style['display'] = 'none'
+        offers[k].classList.replace("shop-box-spinner-item", "shop-box-spinner-item--inactive")
+        offers[k].style['display'] = 'none'
         dots[k].style['background-color'] = '#D5F8E6'
     }
 
-    images[(currentIndex + j - 1) % j].style['display'] = 'flex'
+    offers[(currentIndex + j - 1) % j].classList.replace("shop-box-spinner-item--inactive", "shop-box-spinner-item")
+    offers[(currentIndex + j - 1) % j].style['display'] = 'flex'
     dots[(currentIndex + j - 1) % j].style['background-color'] = '#a7ccba'
 
     currentIndex = (currentIndex - 1) % j
@@ -46,11 +52,13 @@ function prev() {
 function dot(n) {
 
     for (let k = 0; k < j; k++) {
-        images[k].style['display'] = 'none'
+        offers[k].classList.replace("shop-box-spinner-item", "shop-box-spinner-item--inactive")
+        offers[k].style['display'] = 'none'
         dots[k].style['background-color'] = '#D5F8E6'
     }
 
-    images[n - 1].style['display'] = 'flex'
+    offers[n - 1].classList.replace("shop-box-spinner-item--inactive", "shop-box-spinner-item")
+    offers[n - 1].style['display'] = 'flex'
     dots[n - 1].style['background-color'] = '#a7ccba'
 
     currentIndex = n - 1
