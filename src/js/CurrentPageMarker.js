adb98182c7769e47df8497dec20131a29fec7089
Gvozdoslav <remrostrus2123@yandex.ru>
2022-12-13 00:48:19 +0300

    fix: repair active page icons

--- src/js/CurrentPageMarker.js
+++ src/js/CurrentPageMarker.js
@@ -5,19 +5,19 @@ const pagesIcons = document.getElementsByClassName("shopHeader__pages")[0].getEl
 
 function initPageIcon() {
 
-    if (window.location.pathname === "/web/src/pages/ComparePage.html") {
+    if (window.location.pathname.endsWith("ComparePage.html")) {
         pagesIcons[0].getElementsByTagName("img")[0].src = "/web/src/images/ScalesActiveIcon.svg"
     }
 
-    if (window.location.pathname === "/web/src/pages/FavoritesPage.html") {
+    if (window.location.pathname.endsWith("FavoritesPage.html")) {
         pagesIcons[1].getElementsByTagName("img")[0].src = "/web/src/images/HeartActive.svg"
     }
 
-    if (window.location.pathname === "/web/src/pages/Profile.html") {
+    if (window.location.pathname.endsWith("Profile.html")) {
         pagesIcons[2].getElementsByTagName("img")[0].src = "/web/src/images/ProfileActiveIcon.svg"
     }
 
-    if (window.location.pathname === "/web/src/pages/BasketPage.html") {
+    if (window.location.pathname.endsWith("BasketPage.html")) {
         pagesIcons[3].getElementsByTagName("img")[0].src = "/web/src/images/ShoppingCartActiveIcon.svg"
     }
 }
