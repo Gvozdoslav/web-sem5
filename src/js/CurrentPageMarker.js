5f09a781865d4ecadd1f52b302a27feb00ccbe1f
Gvozdoslav <remrostrus2123@yandex.ru>
2022-12-27 03:55:49 +0300

    fix: change routes for current page marker

--- src/js/CurrentPageMarker.js
+++ src/js/CurrentPageMarker.js
@@ -5,19 +5,20 @@ const pagesIcons = document.getElementsByClassName("shopHeader__pages")[0].getEl
 
 function initPageIcon() {
 
+    console.log(window.location.pathname)
     if (window.location.pathname.endsWith("ComparePage.html")) {
-        pagesIcons[0].getElementsByTagName("img")[0].src = "/web/src/images/ScalesActiveIcon.svg"
+        pagesIcons[0].getElementsByTagName("img")[0].src = "../../src/images/ScalesActiveIcon.svg"
     }
 
     if (window.location.pathname.endsWith("FavoritesPage.html")) {
-        pagesIcons[1].getElementsByTagName("img")[0].src = "/web/src/images/HeartActive.svg"
+        pagesIcons[1].getElementsByTagName("img")[0].src = "../../src/images/HeartActive.svg"
     }
 
     if (window.location.pathname.endsWith("Profile.html")) {
-        pagesIcons[2].getElementsByTagName("img")[0].src = "/web/src/images/ProfileActiveIcon.svg"
+        pagesIcons[2].getElementsByTagName("img")[0].src = "../../src/images/ProfileActiveIcon.svg"
     }
 
     if (window.location.pathname.endsWith("BasketPage.html")) {
-        pagesIcons[3].getElementsByTagName("img")[0].src = "/web/src/images/ShoppingCartActiveIcon.svg"
+        pagesIcons[3].getElementsByTagName("img")[0].src = "../../src/images/ShoppingCartActiveIcon.svg"
     }
 }
